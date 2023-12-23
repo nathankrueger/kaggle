6345189720124dc37229bab806a60b21a2b16733
nathankrueger <natekrueger805@gmail.com>
2023-12-22 18:30:25 -0700

    Get rid of problematic average age assumption.

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -12,24 +12,10 @@ test_csv = base_dir / 'test.csv'
 CABIN_LETTER_DICT = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, None: 8}
 NAME_PREFIX_DICT = {'Mrs.': 1, 'Miss': 2, 'Mr.': 3, 'Dr.': 4, None: 5}
 EMBARKMENT_DICT = {'C': 1, 'Q': 2, 'S': 3, '': 4}
+DEFAULT_AGE = 0.0
 
-def isint(item):
-    try:
-        int(item)
-    except ValueError:
-        return False
-    return True
-
-def get_encoded_ticket(ticketstr: str) -> int:
-    if isint(ticketstr):
-        return int(ticketstr)
-    else:
-        result = 0
-        parts = ticketstr.split()
-        for part in parts:
-            if isint(part):
-                result += int(part)
-        return result
+# avg age in training data: 29.69, with 177 missing entries
+# avg age in test data: 30.27, with 86 missing entries
 
 def parse_dataset(csv_path):
     result = []
@@ -58,10 +44,10 @@ def parse_dataset(csv_path):
                 'pclass': int(row[offset]),
                 'name': f'{row[offset + 1]},{row[offset + 2]}',
                 'sex': 0 if row[offset + 3] == 'male' else 1,
-                'age': float(row[offset + 4]) if len(row[offset + 4]) > 0 else None,
+                'age': float(row[offset + 4]) if len(row[offset + 4]) > 0 else DEFAULT_AGE,
                 'sibsp': int(row[offset + 5]),
                 'parch': int(row[offset + 6]),
-                'ticket': get_encoded_ticket(row[offset + 7]),
+                'ticket': row[offset + 7],
                 'fare': float(row[offset + 8]) if len(row[offset + 8]) > 0 else None,
                 'cabin': row[offset + 9],
                 'embarked': EMBARKMENT_DICT[row[offset + 10].strip()]
@@ -72,11 +58,6 @@ def parse_dataset(csv_path):
 def prepare_dataset_for_model(ds):
     numeric_inputs = []
     numeric_outputs = []
-
-    mean_age, num_missing_age = calculate_mean_for_missing_data(ds, 'age')
-    for passenger in ds:
-        if passenger['age'] is None:
-            passenger['age'] = mean_age
 
     for passenger in ds:
         # encode name prefix
@@ -99,7 +80,7 @@ def prepare_dataset_for_model(ds):
             [
                 passenger['pclass'],
                 passenger['sex'],
-                passenger['age'],
+                #passenger['age'],
                 passenger['sibsp'],
                 passenger['parch'],
                 passenger['embarked'],
@@ -141,13 +122,19 @@ class TitanicHyperModel(kt.HyperModel):
 
     def build(self, hp: kt.HyperParameters) -> keras.Model:
         # hyperparams to test
-        units = hp.Int(name='units', min_value=16, max_value=256, step=16)
-        layers = hp.Int(name='layers', min_value=1, max_value=5, step=1)
-        activation = hp.Choice(name='activation', values=['tanh', 'relu'])
-        layer_size_reduction = hp.Choice('layer_size_reduction_scheme', values=[0.75, 0.5, 0.3])
-        layer_reduction_period = hp.Choice('layer_reduction_period', values=[1, 2, 3])
-        optimizer = hp.Choice(name='optimizer', values=['adam', 'rmsprop'])
-        use_dropout = hp.Boolean(name='use_dropout')
+        units = hp.Int(name='units', min_value=64, max_value=256, step=32, default=192)
+        layers = hp.Int(name='layers', min_value=2, max_value=5, step=1, default=4)
+        activation = hp.Choice(name='activation', values=['tanh', 'relu'], default='tanh')
+        layer_size_reduction = hp.Choice('layer_size_reduction_scheme', values=[0.5, 0.3], default=0.3)
+        layer_reduction_period = hp.Choice('layer_reduction_period', values=[1, 2], default=1)
+        optimizer = hp.Choice(name='optimizer', values=['adam', 'rmsprop'], default='adam')
+        use_dropout = hp.Boolean(name='use_dropout', default=True)
+        l2_regularization_val = hp.Choice(name='l2_regularization', values=[0.0, 0.001, 0.002, 0.003], default=0.0)
+
+        if l2_regularization_val > 0.0:
+            l2_regularization = keras.regularizers.l2(l2_regularization_val)
+        else:
+            l2_regularization = None
 
         # create a sequential model
         model = keras.Sequential([keras.Input(shape=(self.num_inputs,), dtype='float32')])
@@ -156,9 +143,9 @@ class TitanicHyperModel(kt.HyperModel):
         curr_layer_size = units
         hidden_layer_idx = 1
         for _ in range(layers):
-            model.add(keras.layers.Dense(curr_layer_size, activation=activation))
+            model.add(keras.layers.Dense(curr_layer_size, activation=activation, kernel_regularizer=l2_regularization))
             if hidden_layer_idx % layer_reduction_period == 0:
-                curr_layer_size *= layer_size_reduction
+                curr_layer_size = int(curr_layer_size * layer_size_reduction)
         
         if use_dropout:
             model.add(keras.layers.Dropout(0.5))
@@ -181,17 +168,17 @@ Train a simple model with fixed architecture
 def run_single_model(train_inputs, train_outputs, use_regularization=False, use_dropout=False) -> keras.Model:
     # multi-layer perceptron
     x = inputs = keras.Input(shape=(len(train_inputs[0]),), dtype='float32')
-    x = keras.layers.Dense(128, activation='tanh', kernel_regularizer=keras.regularizers.l1(0.002) if use_regularization else None)(x)
-    x = keras.layers.Dense(128, activation='tanh', kernel_regularizer=keras.regularizers.l2(0.002) if use_regularization else None)(x)
-    x = keras.layers.Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001) if use_regularization else None)(x)
-    x = keras.layers.Dense(32, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001 if use_regularization else None))(x)
+    x = keras.layers.Dense(96, activation='tanh', kernel_regularizer=keras.regularizers.l1(0.002) if use_regularization else None)(x)
+    x = keras.layers.Dense(32, activation='tanh', kernel_regularizer=keras.regularizers.l2(0.002) if use_regularization else None)(x)
+    #x = keras.layers.Dense(64, activation='tanh', kernel_regularizer=keras.regularizers.l2(0.001) if use_regularization else None)(x)
+    #x = keras.layers.Dense(32, activation='tanh', kernel_regularizer=keras.regularizers.l2(0.001 if use_regularization else None))(x)
     if use_dropout:
         x = keras.layers.Dropout(0.5)(x)
     outputs = keras.layers.Dense(1, activation='sigmoid')(x)
     model = keras.Model(inputs, outputs)
 
     model.compile(
-        optimizer=keras.optimizers.Adam(learning_rate=0.001),
+        optimizer=keras.optimizers.RMSprop(learning_rate=0.001),
         loss='binary_crossentropy',
         metrics=['accuracy']
     )
@@ -202,11 +189,11 @@ def run_single_model(train_inputs, train_outputs, use_regularization=False, use_
     # train the model
     try:
          history = model.fit(
-            batch_size=64,
+            batch_size=48,
             validation_split=0.3,
             x=train_inputs,
             y=train_outputs,
-            epochs=250,
+            epochs=400,
             callbacks=[
                 keras.callbacks.TensorBoard(str(base_dir / 'tensorboard_logs')),
                 keras.callbacks.ModelCheckpoint(
@@ -215,8 +202,11 @@ def run_single_model(train_inputs, train_outputs, use_regularization=False, use_
                     #monitor='val_accuracy'
                 ),
                 keras.callbacks.ReduceLROnPlateau(
-                    patience=8,
-                    factor=0.75
+                    patience=15,
+                    factor=0.6
+                ),
+                keras.callbacks.EarlyStopping(
+                    patience=30
                 )
             ]
         )
@@ -230,79 +220,72 @@ def run_single_model(train_inputs, train_outputs, use_regularization=False, use_
 """
 Use keras-tuner to sweep across hyperparameter space and determine the best
 configuration hyperparameters for a performant model.
+See: https://keras.io/guides/keras_tuner/getting_started/
 """
-def run_keras_tuner_on_hypermodel(train_inputs, train_outputs, max_trials: int=400, monitor: str='val_accuracy', mode: str='max') -> keras.Model:
+def run_keras_tuner_on_hypermodel(train_inputs, train_outputs, max_trials: int=400, monitor: str='val_accuracy') -> keras.Model:
     hypermodel = TitanicHyperModel(len(train_inputs[0]))
-    val_split = 0.2
-    batch_sz = 64
+    val_split = 0.3
+    batch_sz = 48
+    epochs = 300
 
     tuner = kt.tuners.BayesianOptimization(
         hypermodel=hypermodel,
         objective=monitor,
         max_trials=max_trials,
-        executions_per_trial=2,
+        executions_per_trial=1,
         directory=base_dir / 'titanic_hypertuning',
         overwrite=True
     )
 
     print(tuner.search_space_summary())
 
-    tuner.search(
-        x=train_inputs,
-        y=train_outputs,
-        epochs=250,
-        validation_split=val_split,
-        callbacks=[
-            keras.callbacks.ReduceLROnPlateau(
-                monitor=monitor,
-                patience=6,
-                factor=0.5
-            ),
-            keras.callbacks.EarlyStopping(
-                monitor=monitor,
-                patience=15
-            )
-        ],
-        batch_size=batch_sz
-    )
+    try:
+        tuner.search(
+            x=train_inputs,
+            y=train_outputs,
+            epochs=epochs,
+            validation_split=val_split,
+            callbacks=[
+                keras.callbacks.ReduceLROnPlateau(
+                    monitor=monitor,
+                    patience=8,
+                    factor=0.6
+                ),
+                keras.callbacks.EarlyStopping(
+                    monitor=monitor,
+                    patience=15
+                )
+            ],
+            batch_size=batch_sz
+        )
+    except KeyboardInterrupt:
+        print(os.linesep)
 
-    best_hp = tuner.get_best_hyperparameters(1)[0]
-    print(best_hp)
-    best_model = hypermodel.build(best_hp)
+    # print out the top 5 hyperparameter sets
+    best_hps = tuner.get_best_hyperparameters(5)
+    for idx, best_hp in enumerate(best_hps):
+        print(f'#{idx+1} Best HyperParameter values: {os.linesep}{best_hp.values}')
+
+    # choose and build the best model
+    best_model = hypermodel.build(best_hps[0])
 
     # train the best model architecture (best hyperparams)
     history = best_model.fit(
         x=train_inputs,
         y=train_outputs,
         validation_split=val_split,
-        epochs=200,
+        epochs=epochs,
         callbacks=[
             keras.callbacks.ReduceLROnPlateau(
                 monitor=monitor,
-                patience=6,
-                factor=0.5
+                patience=8,
+                factor=0.6
             ),
             keras.callbacks.EarlyStopping(
                 monitor=monitor,
                 patience=15
             )
         ],
-        batch_size=batch_sz
-    )
-    monitor_metric_per_epoch = history.history[monitor]
-    if mode == 'min':
-        best_epoch = monitor_metric_per_epoch.index(min(monitor_metric_per_epoch)) + 1
-    elif mode == 'max':
-        best_epoch = monitor_metric_per_epoch.index(max(monitor_metric_per_epoch)) + 1
-    else:
-        raise Exception(f'Unsupported mode: {mode}')
-
-    # train one last time with all the data
-    best_model = hypermodel.build(best_hp)
-    best_model.fit(
-        x=train_inputs,
-        y=train_outputs,
-        epochs=int(best_epoch * (1.0 + val_split)),
         batch_size=batch_sz
     )
 
@@ -311,6 +294,7 @@ def run_keras_tuner_on_hypermodel(train_inputs, train_outputs, max_trials: int=4
 if __name__ == '__main__':
     # parse & collect the training data in a usable format
     train_ds = parse_dataset(train_csv)
+    random.seed(1337)
     random.shuffle(train_ds)
     numeric_inputs, numeric_outputs = prepare_dataset_for_model(train_ds)
 
@@ -318,8 +302,8 @@ if __name__ == '__main__':
     train_inputs = np.array(numeric_inputs).astype('float32')
     train_outputs = np.array(numeric_outputs).astype('float32')
 
-    model = run_keras_tuner_on_hypermodel(train_inputs, train_outputs, max_trials=1)
-    #model = run_single_model(train_inputs, train_outputs)
+    #model = run_keras_tuner_on_hypermodel(train_inputs, train_outputs, monitor='val_loss', max_trials=128)
+    model = run_single_model(train_inputs, train_outputs, use_dropout=False, use_regularization=False)
   
     # make the predictions
     test_ds = parse_dataset(test_csv)
